c9748a59e2ed0686d667cf1409b447b5a327c980
luciacp1312 <80093487+luciacp1312@users.noreply.github.com>
2023-03-24 18:29:48 +0100

    Adding pages. Modifying Partnerships page

--- webpage/views.py
+++ webpage/views.py
@@ -10,3 +10,27 @@ views = Blueprint('views', __name__)
 def home():
 
     return render_template("home.html", user=current_user)
+
+@views.route('/frequent_questions', methods=['GET', 'POST'])
+@login_required
+def freq_quest():
+
+    return render_template("freqQuest.html", user=current_user)
+
+@views.route('/vegetables_info', methods=['GET', 'POST'])
+@login_required
+def veg_info():
+
+    return render_template("vegInfo.html", user=current_user)
+
+@views.route('/volunteers', methods=['GET', 'POST'])
+@login_required
+def volunteers():
+
+    return render_template("volunteers.html", user=current_user)
+
+@views.route('/partnerships', methods=['GET', 'POST'])
+@login_required
+def partnerships():
+
+    return render_template("partnerships.html", user=current_user)
