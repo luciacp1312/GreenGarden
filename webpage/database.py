9b0776047dcd4e0e5a4515c4549b95c4b558629f
luciacp1312 <80093487+luciacp1312@users.noreply.github.com>
2023-03-28 19:57:37 +0200

    Volunteers survey done

--- webpage/database.py
+++ webpage/database.py
@@ -1,6 +1,6 @@
 from . import db
 from flask_login import UserMixin # This will give the users some things specific for flask login
-from sqlalchemy.sql import func
+import uuid
 
 # In this class we create our database, that will later be loaded in the instance folder as database.db
 
@@ -11,18 +11,36 @@ class User(db.Model, UserMixin): # UserMixin is needed to access all the informa
     username = db.Column(db.String(150))
     requests = db.relationship('Request') 
     
+
+    
 class Request(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
     city = db.Column(db.String(100), nullable=False)
     country = db.Column(db.String(100), nullable=False)
     postalCode = db.Column(db.String(10), nullable=False)
-    startDate = db.Column(db.Date)
-    endDate = db.Column(db.Date)
-    consider = db.Column(db.String(250))
+    startDate = db.Column(db.Date, nullable=True)
+    endDate = db.Column(db.Date, nullable=True)
+    consider = db.Column(db.String(250), nullable=True)
     userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+def __init__(self, name, email, phone, city, country, postalCode, startDate=None, endDate=None, consider=None, userId=None):
+    self.name = name
+    self.email = email
+    self.phone = phone
+    self.city = city
+    self.country = country
+    self.postalCode = postalCode
+    self.startDate = startDate
+    self.endDate = endDate
+    self.consider = consider
+    self.userId = userId
+    if id is None:
+        self.id = str(uuid.uuid4()) # To create a random ID number for the request
+    else:
+        self.id = id
     
     # 1 user can have 0 or more requests: 1-M relationship
 
--- webpage/views.py
+++ webpage/views.py
@@ -26,6 +26,15 @@ def veg_info():
 
     return render_template("vegInfo.html", user=current_user)
 
+
+def validate_date_string(date_str):
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d').date()
+    except ValueError:
+        flash('Invalid date format', category='error')
+        return None
+    return date
+
 @views.route('/volunteers', methods=['GET', 'POST'])
 @login_required
 def volunteers():
@@ -36,38 +45,52 @@ def volunteers():
         city = request.form.get('city')
         country = request.form.get('country')
         postalCode = request.form.get('postalCode')
-        startDate_str = request.form.get('startDate') # poner que tiene que ser antes que el endDate
+        startDate_str = request.form.get('startDate')
         endDate_str = request.form.get('endDate')
         consider = request.form.get('consider')
+        send = False
            
         startDate = ''
         endDate = ''
-        if not phone.isdigit():
-            flash('Phone number must be an integer', category='error')
-        elif not postalCode.isdigit():
-            flash('Postal code must be an integer', category='error')
-        elif not startDate_str=='' and not endDate_str=='':
-            startDate = datetime.strptime(startDate_str, '%Y-%m-%d').date()
-            endDate = datetime.strptime(endDate_str, '%Y-%m-%d').date()
-            if startDate < datetime.now().date() or endDate < datetime.now().date():
-                flash('The dates have to be set after the current date', category='error')
-            elif startDate > endDate:
-                flash('The start date must be the same or before the end date', category='error')
+        
+        try:
+            phone_int = int(phone)
+            postalCode_int = int(postalCode)
+        except ValueError:
+            flash('Phone number and postal code must be integers', category='error')
         else:
-            id_number = uuid.uuid4() # To create a random ID number for the request
-            
-            # POR ALGÚN MOTIVO, NO ME ESTÁ GUARDANDO EL NAME (SALE COMO NONE)
-            if startDate=='' and endDate=='':
-                new_petition = Request(id=str(id_number), name=name, email=email, phone=phone, city=city, country=country, postalCode=postalCode, consider=consider, userId=current_user.id)
-            elif startDate=='':
-                new_petition = Request(id=str(id_number), name=name, email=email, phone=phone, city=city, country=country, postalCode=postalCode, endDate=endDate, consider=consider, userId=current_user.id)
-            elif endDate=='':
-                new_petition = Request(id=str(id_number), name=name, email=email, phone=phone, city=city, country=country, postalCode=postalCode, startDate=startDate, consider=consider, userId=current_user.id)
+            if startDate_str:
+                try:
+                    startDate = datetime.strptime(startDate_str, '%Y-%m-%d').date()
+                except ValueError:
+                    flash('Invalid start date format', category='error')
+                else:
+                    startDate = datetime.now().date()
             else:
-                new_petition = Request(id=str(id_number), name=name, email=email, phone=phone, city=city, country=country, postalCode=postalCode, startDate=startDate, endDate=endDate, consider=consider, userId=current_user.id)
+                startDate = datetime.now().date()
+
+            if endDate_str:
+                try:
+                    endDate = datetime.strptime(endDate_str, '%Y-%m-%d').date()
+                except ValueError:
+                    flash('Invalid end date format', category='error')
+                else:
+                    endDate = datetime.now().date()
+            else:
+                endDate = startDate
+            
+            if startDate > endDate:
+                flash('The start date must be the same or before the end date', category='error')
+            else:
+                send = True
+            
+        if send:
+            new_petition = Request(id=str(uuid.uuid4()), name=name, email=email, phone=phone, city=city, country=country, postalCode=postalCode, startDate=startDate, endDate=endDate, consider=consider, userId=current_user.id)
             db.session.add(new_petition)
             db.session.commit()
             flash('Your form has been successfully created. An email/phone message will be sent to you as soon as possible with possible dates', category='success')
+        else:
+            flash('There has been a problem with the form', category='error')
     return render_template("volunteers.html", user=current_user)
 
 @views.route('/partnerships', methods=['GET', 'POST'])
